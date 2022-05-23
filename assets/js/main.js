d5066ccc172e10363231f57c5d380b83667818f0
Glecio Santos <glecio.santos@gmail.com>
2022-05-23 08:40:58 -0300

    Change slides per iew for breakpoint 576px

--- assets/js/main.js
+++ assets/js/main.js
@@ -63,7 +63,7 @@ let swiper = new Swiper(".swiper", {
     },
     breakpoints: {
         576: {
-          slidesPerView: 4,
+          slidesPerView: 2,
         },
         768: {
           slidesPerView: 2,
@@ -77,15 +77,16 @@ const sections = document.querySelectorAll('section[id]')
 
 function scrollActive(){
     const scrollY = window.pageYOffset
-
+    
     sections.forEach(current =>{
         const sectionHeight = current.offsetHeight,
               sectionTop = current.offsetTop - 58,
               sectionId = current.getAttribute('id')
-
+              
         if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
             document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.add('active-link')
         }else{
+            console.log(sectionHeight, sectionTop, sectionId)
             document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.remove('active-link')
         }
     })
